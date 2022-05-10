c70689838d920164a2b576d87c88b1abc8aa1ffb
Igor Jose <goncalvesigor67@gmail.com>
2022-05-10 09:14:25 -0300

    definindo metodos de autenticacao

--- src/main/java/br/security/project/app/security/SecurityConfiguration.java
+++ src/main/java/br/security/project/app/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.security.project.app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticationService autenticationService;
 
     // configuracoes de autorizacao
     @Override
@@ -25,7 +30,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // configuracoes de autenticacao
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        // validando a senha criptografia
+        auth.userDetailsService(autenticationService)
+            .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     // configuracoes para recursos estaticos
